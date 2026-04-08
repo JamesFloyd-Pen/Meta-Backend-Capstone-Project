71c98d9af90533309b674f9c0cf859c17f1e070b
James Floyd <Commanderpip@gmail.com>
2026-04-08 13:32:46 -0400

    Quick Hotfix
    relocated the defaultrouter

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,8 +2,11 @@
 from django.urls import path
 from . import views
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.routers import DefaultRouter
 
 
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('', views.index, name='home'),
